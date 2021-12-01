26f5b96678ade22f06393b2e24c96958187c0e61
Dawid <mateusiakdawid@gmail.com>
2021-12-01 16:51:03 +0100

    Method replacing forbidden signs from Spotify title

--- playlist_comparator.py
+++ playlist_comparator.py
@@ -5,6 +5,7 @@ class PlaylistComparator:
     def __init__(self, downloaded_playlist, spotify_playlist):
         self.downloaded_playlist = downloaded_playlist.downloaded_playlist
         self.spotify_playlist = spotify_playlist.playlist
+        self.forbidden_signs = {'<', '>', ':', '"', '/', '\\', '|', '?', '*'}
         self.spotify_song_titles = [song[0] for song in self.spotify_playlist]
         self.spotify_song_ids = [song[1] for song in self.spotify_playlist]
         self.spotify_title_mapping = {}
@@ -28,8 +29,32 @@ class PlaylistComparator:
             print("ALL SONGS ALREADY DOWNLOADED.")
 
     def generate_spotify_titles_mapping(self):
-        for song in self.spotify_song_titles:
-            self.spotify_title_mapping[song.lower().replace] = song
+        """
+        Generates dictionary with processed titles to match directory song title with Spotify title in case
+        of different case of letters or the presence of forbidden signs in file name.
+        """
+        for song_title in self.spotify_song_titles:
+            formatted_song_title = self.replace_forbidden_file_signs(song_title)
+            self.spotify_title_mapping[formatted_song_title.lower()] = formatted_song_title
+
+    def replace_forbidden_file_signs(self, song_title):
+        """
+        Replaces forbidden signs from Spotify song title.
+        """
+        file_signs = set(song_title)
+        new_title = song_title
+        found_forbidden_signs = file_signs.intersection(self.forbidden_signs)
+        if found_forbidden_signs:
+            for forbidden_sign in found_forbidden_signs:
+                if forbidden_sign == '?':
+                    new_title = new_title.replace(forbidden_sign, '')
+                elif forbidden_sign == '"':
+                    new_title = new_title.replace(forbidden_sign, "'")
+                else:
+                    new_title = new_title.replace(forbidden_sign, '-')
+        return new_title
+
+
 
     def get_not_liked_songs(self):
         for song in self.downloaded_playlist:
