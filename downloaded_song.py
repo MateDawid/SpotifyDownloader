c62b668ddbdd5c10ad12a2e4fcb3e3ef16edcff8
Dawid <mateusiakdawid@gmail.com>
2021-12-05 22:04:25 +0100

    Add DownloadedSong class

--- downloaded_song.py
+++ downloaded_song.py
@@ -1,34 +1,14 @@
-class SpotifySong:
-    def __init__(self, track_data):
-        self.track_data = track_data
-        self.name = self.set_name()
-        self.artists = self.set_artists()
-        self.release_date = self.set_release_date()
-        self.track_number = self.set_track_number()
-        self.url = self.set_url()
-
-    def set_name(self):
-        return self.track_data['name']
-
-    def set_artists(self):
-        return [artist['name'] for artist in self.track_data['artists']]
-
-    def set_release_date(self):
-        return self.track_data['album']['release_date']
-
-    def set_track_number(self):
-        return self.track_data['track_number']
-
-    def set_url(self):
-        return self.track_data['external_urls']['spotify']
-
-
 class DownloadedSong:
     def __init__(self, filename):
         self.filename = filename
+        self.name = self.set_name()
+        self.artists = self.set_artists()
+
+    def __repr__(self):
+        return f'{", ".join(self.artists)} - {self.name}'
 
     def set_name(self):
+        return ('-'.join(self.filename.split('-')[1:])).strip()
 
     def set_artists(self):
-        pass
-    
+        return [artist.strip() for artist in self.filename.split('-')[0].split(',')]
