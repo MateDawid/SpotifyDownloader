d2f3d2ea6dcf8ec772ba8dce35cda9b266f96c07
Dawid <mateusiakdawid@gmail.com>
2021-12-07 15:44:32 +0100

    Saving SpotifyAPI credentials in separate file, methods to manipulate credentials

--- spotify_client.py
+++ spotify_client.py
@@ -1,45 +1,74 @@
 from math import ceil
+import os
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyOAuth, SpotifyOauthError
 
 from spotify_song import SpotifySong
 
 
-CLIENT_ID = '6351cad0cd984110b83b21c1b4d4bce7'
-CLIENT_SECRET = '5f793eda0269453d94ff8a1d822b9640'
-
-
 class SpotifyClient:
     def __init__(self):
+        self.credentials = self.get_credentials()
         self.user = None
-        self.connect_with_spotify_user(CLIENT_ID, CLIENT_SECRET)  # TODO
-        self.spotify_playlist = self.set_playlist()
+        self.redirect_uri = 'http://localhost:8000'
+        self.favourite_playlist = []
+        if self.credentials:
+            self.connect_with_spotify_user()
 
-    def connect_with_spotify_user(self, client_id, client_secret, redirect_uri='http://localhost:8000'):
-        auth_manager = SpotifyOAuth(
-            client_id=client_id,
-            client_secret=client_secret,
-            redirect_uri=redirect_uri,
-            scope="user-library-read user-library-modify"
-        )
-        self.user = spotipy.Spotify(auth_manager=auth_manager)
+    def set_redirect_uri(self, redirect_uri):
+        self.redirect_uri = redirect_uri
+
+    def save_credentials(self, client_id, client_secret):
+        if os.path.isfile('.env'):
+            with open('.env', 'r+') as env_file:
+                env_file.truncate(0)
+        with open('.env', 'w') as env_file:
+            env_file.write(f"CLIENT_ID={client_id}\n")
+            env_file.write(f"CLIENT_SECRET={client_secret}")
+        self.connect_with_spotify_user()
+
+    @staticmethod
+    def get_credentials():
+        credentials = {}
+        if os.path.isfile('.env'):
+            with open('.env') as env_file:
+                for line in env_file:
+                    key, value = line.strip().split('=', 1)
+                    credentials[key.strip()] = value.strip()
+        return credentials
+
+    def connect_with_spotify_user(self):
+        credentials = self.get_credentials()
+        if credentials:
+            auth_manager = SpotifyOAuth(
+                client_id=credentials['CLIENT_ID'],
+                client_secret=credentials['CLIENT_SECRET'],
+                redirect_uri=self.redirect_uri,
+                scope="user-library-read user-library-modify"
+            )
+            self.user = spotipy.Spotify(auth_manager=auth_manager)
+            try:
+                self.get_favourite_playlist()
+            except SpotifyOauthError:
+                print("Wrong SpotifyAPI credentials proviced!")
 
     def get_songs_sublist(self, offset=0, limit=50):
         return self.user.current_user_saved_tracks(limit=limit, offset=offset)
 
-    def set_playlist(self, limit_step=50):
+    def get_favourite_playlist(self, limit_step=50):
         playlist = []
-        max_range = int(ceil(self.get_songs_sublist(limit=1)['total']/50)*50)
-        for offset in range(0, max_range, limit_step):
-            results = self.get_songs_sublist(offset=offset, limit=limit_step)
-            for item in results['items']:
-                playlist.append(SpotifySong(item['track']))
-        return playlist
+        if self.user:
+            max_range = int(ceil(self.get_songs_sublist(limit=1)['total']/50)*50)
+            for offset in range(0, max_range, limit_step):
+                results = self.get_songs_sublist(offset=offset, limit=limit_step)
+                for item in results['items']:
+                    playlist.append(SpotifySong(item['track']))
+        self.favourite_playlist = playlist
 
-    def print_playlist(self):
-        if self.spotify_playlist:
-            count = len(self.spotify_playlist)
-            for idx, song in enumerate(self.spotify_playlist, start=1):
+    def print_favourite_playlist(self):
+        if self.get_favourite_playlist:
+            count = len(self.favourite_playlist)
+            for idx, song in enumerate(self.favourite_playlist, start=1):
                 print(f"({idx}/{count}) {song}")
         else:
-            print(f"No songs found in playlist!")
+            print(f"No songs in Spotify favourite playlist!")
